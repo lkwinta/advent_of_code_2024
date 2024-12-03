a31fbd7f153752bd285edb060186ec76a8a65ea4
Łukasz Kwinta <lukaszmind@gmail.com>
2024-12-03 10:03:49 +0100

    fix regex parenthesis

--- lib/advent_of_code_2024/solutions/y24/day03.ex
+++ lib/advent_of_code_2024/solutions/y24/day03.ex
@@ -2,17 +2,17 @@ defmodule AdventOfCode2024.Solutions.Y24.Day03 do
   alias AoC.Input
 
   def parse(input, :part_one) do
-    Regex.scan(~r"mul\(\d{1,3},\d{1,3}\)", Input.read!(input), capture: :first)
+    Regex.scan(~r"mul\(\d{1,3},\d{1,3}\)", Input.read!(input))
     |> Enum.map(&parse_str/1)
   end
 
   def parse(input, :part_two) do
-    Regex.scan(~r"mul\(\d{1,3},\d{1,3}\)|don't()|do()", Input.read!(input), capture: :first)
+    Regex.scan(~r"mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\)", Input.read!(input))
     |> Enum.map(&parse_str/1)
   end
 
-  defp parse_str(["don't"]), do: :dont
-  defp parse_str(["do"]), do: :do
+  defp parse_str(["don't()"]), do: :dont
+  defp parse_str(["do()"]), do: :do
   defp parse_str([mul_expr]) do
     [[x], [y]] = Regex.scan(~r"\d+", mul_expr)
     {x, _} = Integer.parse(x)
