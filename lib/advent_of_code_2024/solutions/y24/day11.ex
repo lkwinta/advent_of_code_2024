cdcaf9096201309a1916708170f88646fd86cba2
Łukasz Kwinta <lukaszmind@gmail.com>
2024-12-11 10:32:25 +0100

    day 11 memoization

--- lib/advent_of_code_2024/solutions/y24/day11.ex
+++ lib/advent_of_code_2024/solutions/y24/day11.ex
@@ -1,43 +1,55 @@
 defmodule AdventOfCode2024.Solutions.Y24.Day11 do
   alias AoC.Input
+  use Agent
 
   def parse(input, _part) do
     Input.read!(input)
     |> String.split([" ", "\n"])
-    |> Enum.filter(& &1 != "")
+    |> Enum.filter(&(&1 != ""))
     |> Enum.map(&Integer.parse/1)
-    |> Enum.map(& elem(&1, 0))
-    |> IO.inspect()
+    |> Enum.map(&elem(&1, 0))
   end
 
   def part_one(problem) do
-    problem |> iterate(25, &blink/1) |> length()
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+
+    problem
+    |> Stream.map(&blink(&1, 25))
+    |> Enum.sum()
   end
 
   def part_two(problem) do
-    problem |> iterate(75, &blink/1) |> length()
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+
+    problem
+    |> Stream.map(&blink(&1, 75))
+    |> Enum.sum()
   end
 
-  def iterate(list, num, _) when num == 0, do: list
-  def iterate(list, num, fun) when num > 0 do
-    iterate(fun.(list), num - 1, fun)
-  end
+  defp blink(num, blink_num) do
+    memo_value = Agent.get(__MODULE__, &Map.get(&1, {num, blink_num}))
 
-  defp blink(list), do: blink(list, [])
+    cond do
+      memo_value ->
+        memo_value
 
-  defp blink([], result), do: result
-  defp blink([0 | list], result), do: blink(list, [1 | result])
-  defp blink([element | list], result) do
-    if rem(length(Integer.digits(element)), 2) != 0 do
-      blink(list, [element*2024 | result])
-    else
-      digits = Integer.digits(element)
-      half = div(length(digits), 2)
+      blink_num == 0 ->
+        1
 
-      left_half = Integer.undigits(digits |> Enum.take(half))
-      right_half = Integer.undigits(digits |> Enum.drop(half))
+      num == 0 ->
+        blink(1, blink_num - 1)
 
-      blink(list, [left_half, right_half | result])
+      rem(length(Integer.digits(num)), 2) != 0 ->
+        blink(num * 2024, blink_num - 1)
+
+      true ->
+        digits = div(length(Integer.digits(num)), 2)
+        half = :math.pow(10, digits) |> round
+
+        res = blink(div(num, half), blink_num - 1) + blink(rem(num, half), blink_num - 1)
+
+        Agent.update(__MODULE__, &Map.put(&1, {num, blink_num}, res))
+        res
     end
   end
 end
